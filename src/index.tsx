d68bfd10327dd2c593c3d0c8cdec52fe747ebcb1
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2023-09-18 21:48:10 -0700

    add basic shit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    //errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+        //errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
+root.render(<RouterProvider router={router} />);

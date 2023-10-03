22451ec1ba6d345bf85fe3a705ba556a56d319cb
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2023-10-03 13:15:56 -0700

    wip profile cards

--- src/Components/Cards/ProfileCard/index.tsx
+++ src/Components/Cards/ProfileCard/index.tsx
@@ -1,12 +1,18 @@
+import './styles.css';
+
 interface ProfileCardTypes {
   profileImage: string
+  profileDescription: string
 }
 
-export const ProfileCard = ({ profileImage }: ProfileCardTypes) => {
+export const ProfileCard = ({ profileImage, profileDescription }: ProfileCardTypes) => {
   return (
-    <div className="w-full bg-sp-primary-400 flex items-center justify-evenly">
-      <div className="w-1/3">
-        <img src={profileImage} />
+    <div className="profile-card-container w-full p-4 bg-sp-tertiary-500 rounded-lg flex items-center justify-evenly flex-col lg:flex-row">
+      <div className="w-1/3 justify-center flex">
+        <img className=" h-48 w-96 object-cover lg:w-48 " src={profileImage} />
+      </div>
+      <div className="w-full h-96">
+        <p className="text-sp-primary-200">{profileDescription}</p>
       </div>
     </div>
   )
--- src/Pages/Drivers/index.tsx
+++ src/Pages/Drivers/index.tsx
@@ -1,9 +1,13 @@
 import { ProfileCard } from "../../Components/Cards/ProfileCard"
+import Dillon from "../../images/Dillon-Racer.jpg"
 
 export const Drivers = ({ }) => {
   return (
-    <div className="container flex justify-center items-center">
-      <ProfileCard />
+    <div className=" mx-auto container flex flex-col justify-center items-center gap-10">
+      <ProfileCard profileImage={Dillon} profileDescription="he drives the boats" />
+      <ProfileCard profileImage={Dillon} profileDescription="he drives the boats" />
+      <ProfileCard profileImage={Dillon} profileDescription="he drives the boats" />
+      <ProfileCard profileImage={Dillon} profileDescription="he drives the boats" />
     </div>
   )
 }
